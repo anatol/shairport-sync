ee19f3b3779b16dd9669f707b656d1aa609ce69d
Anatol Pomozov <anatol.pomozov@gmail.com>
2015-05-26 09:03:39 -0700

    clang-format code formatting
    find . \( -name \*.cpp -or -name \*.h \) -exec clang-format -i -style='{BasedOnStyle: llvm, ColumnLimit: 100}' '{}' \;

--- common.h
+++ common.h
@@ -8,65 +8,64 @@
 #include "mdns.h"
 
 #if defined(__APPLE__) && defined(__MACH__)
-	/* Apple OSX and iOS (Darwin). ------------------------------ */
+/* Apple OSX and iOS (Darwin). ------------------------------ */
 #include <TargetConditionals.h>
 #if TARGET_OS_MAC == 1
-	/* OSX */
-	#define COMPILE_FOR_OSX	1
+/* OSX */
+#define COMPILE_FOR_OSX 1
 #endif
 #endif
 
 #if defined(__linux__)
-	/* Linux. --------------------------------------------------- */
-	#define COMPILE_FOR_LINUX	1
+/* Linux. --------------------------------------------------- */
+#define COMPILE_FOR_LINUX 1
 #endif
 
 // struct sockaddr_in6 is bigger than struct sockaddr. derp
 #ifdef AF_INET6
-    #define SOCKADDR struct sockaddr_storage
-    #define SAFAMILY ss_family
+#define SOCKADDR struct sockaddr_storage
+#define SAFAMILY ss_family
 #else
-    #define SOCKADDR struct sockaddr
-    #define SAFAMILY sa_family
+#define SOCKADDR struct sockaddr
+#define SAFAMILY sa_family
 #endif
 
-
 enum stuffing_type {
-  ST_basic        = 0,
+  ST_basic = 0,
   ST_soxr,
 } type;
 
-
-
 typedef struct {
-    char *password;
-    char *apname;
-    uint8_t hw_addr[6];
-    int port;
-    int resyncthreshold; // if it get's out of whack my more than this, resync. Zero means never resync.
-    int timeout; // while in play mode, exit if no packets of audio come in for more than this number of seconds . Zero means never exit.
-    char *output_name;
-    audio_output *output;
-    char *mdns_name;
-    mdns_backend *mdns;
-    int buffer_start_fill;
-    uint32_t latency;
-    uint32_t userSuppliedLatency; // overrides all other latencies -- use with caution
-    uint32_t iTunesLatency; // supplied with --iTunesLatency option
-    uint32_t AirPlayLatency; //supplied with --AirPlayLatency option
-    uint32_t ForkedDaapdLatency; //supplied with --ForkedDaapdLatency option    
-    int daemonise;
-    int statistics_requested;
-    char *cmd_start, *cmd_stop;
-    int tolerance; // allow this much drift before attempting to correct it
-    int cmd_blocking;
-    enum stuffing_type packet_stuffing;
-    char *pidfile;
-    char *logfile;
-    char *errfile;
+  char *password;
+  char *apname;
+  uint8_t hw_addr[6];
+  int port;
+  int resyncthreshold; // if it get's out of whack my more than this, resync. Zero means never
+                       // resync.
+  int timeout; // while in play mode, exit if no packets of audio come in for more than this number
+               // of seconds . Zero means never exit.
+  char *output_name;
+  audio_output *output;
+  char *mdns_name;
+  mdns_backend *mdns;
+  int buffer_start_fill;
+  uint32_t latency;
+  uint32_t userSuppliedLatency; // overrides all other latencies -- use with caution
+  uint32_t iTunesLatency;       // supplied with --iTunesLatency option
+  uint32_t AirPlayLatency; // supplied with --AirPlayLatency option
+  uint32_t ForkedDaapdLatency; // supplied with --ForkedDaapdLatency option
+  int daemonise;
+  int statistics_requested;
+  char *cmd_start, *cmd_stop;
+  int tolerance; // allow this much drift before attempting to correct it
+  int cmd_blocking;
+  enum stuffing_type packet_stuffing;
+  char *pidfile;
+  char *logfile;
+  char *errfile;
 } shairport_cfg;
 
-//true if Shairport Sync is supposed to be sending output to the output device, false otherwise
+// true if Shairport Sync is supposed to be sending output to the output device, false otherwise
 
 int get_requested_connection_state_to_output();
 
@@ -82,10 +81,11 @@ uint8_t *base64_dec(char *input, int *outlen);
 char *base64_enc(uint8_t *input, int length);
 
 #define RSA_MODE_AUTH (0)
-#define RSA_MODE_KEY  (1)
+#define RSA_MODE_KEY (1)
 uint8_t *rsa_apply(uint8_t *input, int inlen, int *outlen, int mode);
 
-// given a volume (0 to -30) and high and low attenuations in dB*100 (e.g. 0 to -6000 for 0 to -60 dB), return an attenuation depending on the transfer function
+// given a volume (0 to -30) and high and low attenuations in dB*100 (e.g. 0 to -6000 for 0 to -60
+// dB), return an attenuation depending on the transfer function
 double vol2attn(double vol, long max_db, long min_db);
 
 // return a monolithic (always increasing) time in nanoseconds
--- mdns.h
+++ mdns.h
@@ -8,13 +8,13 @@ void mdns_register(void);
 void mdns_ls_backends(void);
 
 typedef struct {
-    char *name;
-    int (*mdns_register)(char *apname, int port);
-    void (*mdns_unregister)(void);
+  char *name;
+  int (*mdns_register)(char *apname, int port);
+  void (*mdns_unregister)(void);
 } mdns_backend;
 
-#define MDNS_RECORD_WITHOUT_METADATA "tp=UDP", "sm=false", "ek=1", "et=0,1", "cn=0,1", "ch=2", \
-                "ss=16", "sr=44100", "vn=3", "txtvers=1", \
-                config.password ? "pw=true" : "pw=false"
+#define MDNS_RECORD_WITHOUT_METADATA                                                               \
+  "tp=UDP", "sm=false", "ek=1", "et=0,1", "cn=0,1", "ch=2", "ss=16", "sr=44100", "vn=3",           \
+      "txtvers=1", config.password ? "pw=true" : "pw=false"
 
 #endif // _MDNS_H
